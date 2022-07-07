5fcecf3d337c947b12171ac6c457f0c62f82d528
WorthlessProgrammer <guapitorecokie39@gmail.com>
2022-07-07 20:42:40 +0200

    Created a window and removed glew

--- main.c
+++ main.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-#include <GL/glew.h>
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
 
 #define WIDTH 800
 #define HEIGTH 200
 
-int main(int argc, char **argv)
+int main(void)
 {
-	(void) argc;
-	(void) argv;
-
 	if (!glfwInit())
 	{
 		fprintf(stderr, "ERROR: Could not load GLFW3\n");
 		return -1;
 	}
+
+	GLFWwindow* window;
+	window = glfwCreateWindow(WIDTH, HEIGTH, "PongC", NULL, NULL);
+
+	glfwMakeContextCurrent(window);
+	
+
+	while(true)
+	{
+		glfwSwapBuffers(window);
+	}
+
 	return 0;
 }
 
