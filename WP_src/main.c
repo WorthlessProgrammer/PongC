7b4ccbb52116a61e423d3b0e9a1651b3f0e1fee4
WorthlessProgrammer <guapitorecokie39@gmail.com>
2022-07-09 21:33:08 +0200

    Added ball animation movement.

--- WP_src/main.c
+++ WP_src/main.c
@@ -7,8 +7,8 @@
 #include <GL/freeglut.h>   
 #include <GL/freeglut_std.h>
 
-#define WIDTH 800
-#define HEIGHT 600
+#define WIDTH 960
+#define HEIGHT 480
 
 #define WIN_POS_X 100
 #define WIN_POS_Y 100
@@ -20,18 +20,11 @@
 #define RECT_LENGTH 0.5f
 #define RECT_STEP 0.1f
 
-#define BALL_CX 0.0f
+#define BALL_CX 0.0f 
 #define BALL_CY 0.0f
-#define BALL_RAD 0.5f
 #define PI 3.1415926f
 
 #define FPS 1000 / 60
-
-typedef struct {
-	float cx;
-	float cy;
-	float r;
-} Ball;
 
 typedef struct {
 	float x;
@@ -40,21 +33,10 @@ typedef struct {
 	float length;
 } Rectangle;
 
-Ball make_ball(float x, float y, float radius)
-{
-	Ball b = {
-		.cx = x,
-		.cy = y,
-		.r  = radius
-	};
-
-	return b;
-}
-
 Rectangle make_rect(float tl_x, float tl_y)
 {
 	Rectangle r = {
-		.x = tl_x,  
+		.x = tl_x,	
 		.y = tl_y,
 		.width = RECT_WIDTH,
 		.length = RECT_LENGTH
@@ -63,63 +45,74 @@ Rectangle make_rect(float tl_x, float tl_y)
 	return r;
 }
 
-void drawFilledSun()
-{
-    //static float angle;
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
-    glTranslatef(0, 0, -10);
-    int i, x, y;
-    double radius = 0.30;
-    //glColor3ub(253, 184, 19);     
-    glColor3ub(255, 0, 0);
-    double twicePi = 2.0 * 3.142;
-    x = 0, y = 0;
-    glBegin(GL_TRIANGLE_FAN); //BEGIN CIRCLE
-    glVertex2f(x, y); // center of circle
-    for (i = 0; i <= 20; i++)   {
-        glVertex2f (
-            (x + (radius * cos(i * twicePi / 20))), (y + (radius * sin(i * twicePi / 20)))
-            );
-    }
-    glEnd(); //END
-}
-
 void render_rect_uniform_color(Rectangle rect)
 {
-    glBegin(GL_POLYGON);
-    glColor3f(0, 1, 0); // green
-    glVertex2f(rect.x, rect.y);
-    glVertex2f(rect.x + rect.width, rect.y);
-    glVertex2f(rect.x + rect.width, rect.y - rect.length);
-    glVertex2f(rect.x, rect.y - rect.length); 
-    glEnd(); 
+	glBegin(GL_POLYGON);
+	glColor3f(0, 1, 0); // green
+	glVertex2f(rect.x, rect.y);
+	glVertex2f(rect.x + rect.width, rect.y);
+	glVertex2f(rect.x + rect.width, rect.y - rect.length);
+	glVertex2f(rect.x, rect.y - rect.length); 
+	glEnd(); 
 }
 
+void circle(float x, float y)
+{
+	GLfloat radius;
+	int triangleAmount = 20;
+	GLfloat twicePi = 2.0f * PI;
+
+	glBegin(GL_TRIANGLE_FAN);
+	glColor3f(0, 1, 0);
+	radius = 0.02f;
+
+	glVertex2f(x, y); // center of circle
+	for(int i = 0; i <= triangleAmount; i++) {
+		glVertex2f(
+			x + (radius*cos(i * twicePi / triangleAmount)),
+			y + (radius*2*sin(i * twicePi / triangleAmount))
+			);
+	}
+	glEnd();
+}
+
+static float circ_x = BALL_CX;
+static float circ_y = BALL_CY;
 static float lrect_y = RECT_TOP_LEFT_Y;
 static float rrect_y = RECT_TOP_LEFT_Y;
-void display2() 
-{ 
-    glClearColor(0, 0, 0, 1);  
-    glClear(GL_COLOR_BUFFER_BIT);
 
-    Rectangle r = make_rect(RECT_TOP_LEFT_X, lrect_y);
-    Rectangle l = make_rect(-1.0f*RECT_TOP_LEFT_X - RECT_WIDTH, rrect_y); 
+void circ_mv(void)
+{
+	static bool is_right = false;
+	static bool is_left = false;
 
-	/* Ball b = make_ball(BALL_CX, BALL_CY, BALL_RAD); */
-
-    render_rect_uniform_color(r);
-    render_rect_uniform_color(l);
-
-    glutSwapBuffers(); 
+	if (circ_x >= 1) is_right = true;
+	if (circ_x <= -1) is_left = true;
+	if (!is_right) circ_x += 0.007f;
+	else if (is_right && !is_left) circ_x -= 0.007;
+	else
+	{
+		is_right = false;
+		is_left = false;
+	}
+	glutPostRedisplay();
 }
 
-void display()
-{
-    glClearColor(0, 0, 0, 1);  
-    glClear(GL_COLOR_BUFFER_BIT);
+void display() 
+{ 
+	glClearColor(0, 0, 0, 1);  
+	glClear(GL_COLOR_BUFFER_BIT);
 
-	drawFilledSun();
+	Rectangle r = make_rect(RECT_TOP_LEFT_X, lrect_y);
+	Rectangle l = make_rect(-1.0f*RECT_TOP_LEFT_X - RECT_WIDTH, rrect_y); 
+
+	circ_mv();
+	circle(circ_x, circ_y);
+
+	render_rect_uniform_color(r);
+	render_rect_uniform_color(l);
+
+	glutSwapBuffers(); 
 }
 
 float clampf(float a)
@@ -131,34 +124,34 @@ float clampf(float a)
 
 void keyboard_handler(unsigned char key, int x, int y)
 {
-    (void) x;
-    (void) y;
+	(void) x;
+	(void) y;
 
-    switch (key) 
-    {
+	switch (key) 
+	{
 		case 'q': {exit(0); break;} //quit
 		case 's': {lrect_y = clampf(lrect_y - RECT_STEP); break;} //S
 		case 'w': {lrect_y = clampf(lrect_y + RECT_STEP); break;} //w
 		case 'd': {rrect_y = clampf(rrect_y - RECT_STEP); break;} //D
 		case 'e': {rrect_y = clampf(rrect_y + RECT_STEP); break;} //E
 		default: break;
-    }
+	}
 	
-    glutPostRedisplay();
+	glutPostRedisplay();
 }
 
 int main(int argc, char** argv) 
 {										  
-    glutInit(&argc, argv);			    // Initialize GLUT and
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);    // Use single color buffer and no depth buffer.
-    glutInitWindowSize(WIDTH, HEIGHT);		    // Size of display area, in pixels.
-    glutInitWindowPosition(WIN_POS_X, WIN_POS_Y);   // Location of window in screen coordinates.
-    glutCreateWindow("PongC");			    // Parameter is window title. 
-    glutDisplayFunc(display);			    // Called when the window needs to be redrawn.
-    glutKeyboardFunc(keyboard_handler);
+	glutInit(&argc, argv);				// Initialize GLUT and
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);	// Use single color buffer and no depth buffer.
+	glutInitWindowSize(WIDTH, HEIGHT);			// Size of display area, in pixels.
+	glutInitWindowPosition(WIN_POS_X, WIN_POS_Y);	// Location of window in screen coordinates.
+	glutCreateWindow("PongC");				// Parameter is window title. 
+	glutDisplayFunc(display);				// Called when the window needs to be redrawn.
+	glutKeyboardFunc(keyboard_handler);
 
-    glutMainLoop(); // Run the event loop!  This function does not return.
+	glutMainLoop(); // Run the event loop!	This function does not return.
 					// Program ends when user closes the window.
-    return 0;
+	return 0;
 }
 
