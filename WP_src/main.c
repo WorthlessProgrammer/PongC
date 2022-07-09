23e3bda853ee8d278b82f2052bac50b421dc42ac
WorthlessProgrammer <guapitorecokie39@gmail.com>
2022-07-09 18:07:28 +0200

    Changed a few lines of code please devinchi notice that i renamed diplay to display2 for debugging purposes.

--- WP_src/main.c
+++ WP_src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
 #include <GL/gl.h>
 #include <GL/freeglut.h>   
@@ -12,14 +13,25 @@
 #define WIN_POS_X 100
 #define WIN_POS_Y 100
 
-#define RECT_OFFSET 0.03
+#define RECT_OFFSET 0.03f
 #define RECT_TOP_LEFT_X (RECT_OFFSET - 1)
 #define RECT_TOP_LEFT_Y 1
-#define RECT_WIDTH 0.035
-#define RECT_LENGTH 0.5
+#define RECT_WIDTH 0.035f
+#define RECT_LENGTH 0.5f
 #define RECT_STEP 0.1f
 
+#define BALL_CX 0.0f
+#define BALL_CY 0.0f
+#define BALL_RAD 0.5f
+#define PI 3.1415926f
+
 #define FPS 1000 / 60
+
+typedef struct {
+	float cx;
+	float cy;
+	float r;
+} Ball;
 
 typedef struct {
 	float x;
@@ -27,6 +39,17 @@ typedef struct {
 	float width;
 	float length;
 } Rectangle;
+
+Ball make_ball(float x, float y, float radius)
+{
+	Ball b = {
+		.cx = x,
+		.cy = y,
+		.r  = radius
+	};
+
+	return b;
+}
 
 Rectangle make_rect(float tl_x, float tl_y)
 {
@@ -38,6 +61,28 @@ Rectangle make_rect(float tl_x, float tl_y)
 	};
 	
 	return r;
+}
+
+void drawFilledSun()
+{
+    //static float angle;
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    glTranslatef(0, 0, -10);
+    int i, x, y;
+    double radius = 0.30;
+    //glColor3ub(253, 184, 19);     
+    glColor3ub(255, 0, 0);
+    double twicePi = 2.0 * 3.142;
+    x = 0, y = 0;
+    glBegin(GL_TRIANGLE_FAN); //BEGIN CIRCLE
+    glVertex2f(x, y); // center of circle
+    for (i = 0; i <= 20; i++)   {
+        glVertex2f (
+            (x + (radius * cos(i * twicePi / 20))), (y + (radius * sin(i * twicePi / 20)))
+            );
+    }
+    glEnd(); //END
 }
 
 void render_rect_uniform_color(Rectangle rect)
@@ -53,18 +98,28 @@ void render_rect_uniform_color(Rectangle rect)
 
 static float lrect_y = RECT_TOP_LEFT_Y;
 static float rrect_y = RECT_TOP_LEFT_Y;
-void display() 
+void display2() 
 { 
     glClearColor(0, 0, 0, 1);  
     glClear(GL_COLOR_BUFFER_BIT);
 
     Rectangle r = make_rect(RECT_TOP_LEFT_X, lrect_y);
     Rectangle l = make_rect(-1.0f*RECT_TOP_LEFT_X - RECT_WIDTH, rrect_y); 
-    
+
+	/* Ball b = make_ball(BALL_CX, BALL_CY, BALL_RAD); */
+
     render_rect_uniform_color(r);
     render_rect_uniform_color(l);
 
     glutSwapBuffers(); 
+}
+
+void display()
+{
+    glClearColor(0, 0, 0, 1);  
+    glClear(GL_COLOR_BUFFER_BIT);
+
+	drawFilledSun();
 }
 
 float clampf(float a)
@@ -73,7 +128,7 @@ float clampf(float a)
 	if (a - RECT_LENGTH < -1.0f) return -1.0f + RECT_LENGTH;
 	return a;
 }
-//113 119 115
+
 void keyboard_handler(unsigned char key, int x, int y)
 {
     (void) x;
