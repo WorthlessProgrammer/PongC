2c13e31930da47672ffcbe8c25bef51515709e9f
WorthlessProgrammer <guapitorecokie39@gmail.com>
2022-07-09 14:25:14 +0200

    Added Rectangle, RGB, abstractions.

--- WP_src/main.c
+++ WP_src/main.c
@@ -1,57 +1,66 @@
-#include <GL/freeglut_std.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 #include <GL/gl.h>
 #include <GL/freeglut.h>   
+#include <GL/freeglut_std.h>
 
 #define WIDTH 800
 #define HEIGHT 600
+
+#define WIN_POS_X 100
+#define WIN_POS_Y 100
+
+#define RECT_OFFSET 0.05
+#define RECT_TOP_LEFT_X RECT_OFFSET - 1
+#define RECT_TOP_LEFT_Y 1
+#define RECT_WIDTH 0.075
+#define RECT_LENGTH 0.5
  
 void keyboard_handler(unsigned char key, int x, int y)
 {
 	(void) x;
 	(void) y;
 
-    if(key == (unsigned char) 'q')
+    if (key == (unsigned char) 'q')
 	{
         exit(0);
 	}
 }
 
-void display() {  
+void display() 
+{ 
  
     glClearColor(0, 0, 0, 1);  // (In fact, this is the default.)
     glClear(GL_COLOR_BUFFER_BIT);
     
-    glBegin(GL_TRIANGLES);
-    glColor3f(1, 0, 0); // red
-    glVertex2f(-0.8, -0.8);
+    glBegin(GL_POLYGON);
+    glColor3f(1, 0, 0); // red 
+    glVertex2f(RECT_TOP_LEFT_X, RECT_TOP_LEFT_Y);
     glColor3f(0, 1, 0); // green
-    glVertex2f(0.8, -0.8);
+    glVertex2f(RECT_TOP_LEFT_X + RECT_WIDTH, RECT_TOP_LEFT_Y);
     glColor3f(0, 0, 1); // blue
-    glVertex2f(0, 0.9);
-    glEnd(); 
-    
+	glVertex2f(RECT_TOP_LEFT_X + RECT_WIDTH, RECT_TOP_LEFT_Y - RECT_LENGTH);
+    glColor3f(1, 1, 1); // all
+	glVertex2f(RECT_TOP_LEFT_X, RECT_TOP_LEFT_Y - RECT_LENGTH); 
+	glEnd(); 
+    	
     glutSwapBuffers(); // Required to copy color buffer onto the screen.
- 
 }
 
-
-int main( int argc, char** argv ) {  // Initialize GLUT and 
-
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE);    // Use single color buffer and no depth buffer.
+int main(int argc, char** argv) 
+{										  
+    glutInit(&argc, argv);               // Initialize GLUT and
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);    // Use single color buffer and no depth buffer.
     glutInitWindowSize(WIDTH, HEIGHT);   // Size of display area, in pixels.
-    glutInitWindowPosition(100,100);     // Location of window in screen coordinates.
-    glutCreateWindow("PongC");           // Parameter is window title.
-    glutDisplayFunc(display);            // Called when the window needs to be redrawn.
+    glutInitWindowPosition(WIN_POS_X, WIN_POS_Y);     // Location of window in screen coordinates.
+    glutCreateWindow("PongC");           // Parameter is window title. 
+	glutDisplayFunc(display);            // Called when the window needs to be redrawn.
 	glutKeyboardFunc(keyboard_handler);
 
-    glutMainLoop(); // Run the event loop!  This function does not return.
-                    // Program ends when user closes the window.
-    return 0;
-
+	glutMainLoop(); // Run the event loop!  This function does not return.
+					// Program ends when user closes the window.
+	return 0;
 }
 
