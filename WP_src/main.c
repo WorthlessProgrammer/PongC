dbd3b1db8cfaa6e95ee0edaff834e7921aafe1fd
WorthlessProgrammer <guapitorecokie39@gmail.com>
2022-07-10 11:50:30 +0200

    Cleaned Code

--- WP_src/main.c
+++ WP_src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+/* #include <unistd.h> */
 #include <math.h>
 
 #include <GL/gl.h>
@@ -24,8 +25,8 @@
 #define BALL_CY 0.0f
 #define BALL_SPEED 0.007f
 #define BALL_RAD 0.02f
-#define BALL_DIR_X -1.0f
-#define BALL_DIR_Y 0.0f
+#define BALL_DIR_X -0.30f
+#define BALL_DIR_Y 0.70f
 #define BALL_DEF 20
 #define PI 3.1415926f
 
@@ -107,43 +108,20 @@ static float rrect_y = RECT_TOP_LEFT_Y;
 
 void circ_mv(Ball b)
 {
-	/* static bool is_left = false; */
-	/* static bool is_right = true; */
-
-	/* if (circ_x - BALL_RAD <= -1.0f) */ 
-	/* { */
-	/*	circ_x -= BALL_SPEED*b.dir_x; */
-	/*	is_left = true; */
-	/* } */
-	/* if (circ_x + BALL_RAD >= 1.0f) */
-	/* { */
-	/*	circ_x += BALL_SPEED*b.dir_x; */
-	/*	is_right = true; */
-	/* } */
-	/* if (circ_y + BALL_RAD >= 1.0f) circ_y -= BALL_SPEED; */
-	/* if (circ_y - BALL_RAD <= -1.0f) circ_y += BALL_SPEED; */
-	/* if (!is_right) circ_x += BALL_SPEED; */
-	/* else if (is_right && !is_left) circ_x -= BALL_SPEED; */
-	/* else */
-	/* { */
-	/*	is_left = false; */ 
-	/*	is_right = false; */	
-	/* } */
-
 	circ_x += circ_dx*b.velocity;	
 	circ_y += circ_dy*b.velocity;
 	
 	if (circ_x + b.r >= 1.0f || circ_x - b.r <= -1.0f) 
 	{
-		circ_dx *= -1.0f;
-		circ_x += circ_dx*b.velocity;
+		circ_x = 0.0f;
+		circ_y = 0.0f;
 	}
 	if (circ_y + BALL_RAD >= 1.0f || circ_y - BALL_RAD <= -1.0f)
 	{
-		circ_dy *= 1.0f;
+		circ_dy *= -1.0f;
 		circ_y += circ_dy*b.velocity;
 	}
-	/* printf("%f\n", circ_x); */
+
 	glutPostRedisplay();
 }
 
@@ -157,11 +135,11 @@ void display()
 
 	Ball game_ball = make_ball(circ_x, circ_y, BALL_RAD, BALL_SPEED);
 
-	render_circle_uniform_color(game_ball);
-	circ_mv(game_ball);
-
+	render_circle_uniform_color(game_ball);	
 	render_rect_uniform_color(r);
 	render_rect_uniform_color(l);
+
+	circ_mv(game_ball);
 
 	glutSwapBuffers(); 
 }
